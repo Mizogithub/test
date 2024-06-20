6d98565126f5d2f5eb60ebfa6b0dfc681156477b
Mizogithub <hicacchi@gmail.com>
2024-06-20 14:01:31 +0900

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Updatable Userscript
+// @namespace    
+// @version      1.0.0
+// @description  updatable userscript sample.
+// @author       
+// @match        https://example.com/
+// @grant        none
+// @updateURL    
+// @downloadURL  
+// @supportURL   
+// ==/UserScript==
+
+(() => {
+  console.log('v1.1.0');
+})();
